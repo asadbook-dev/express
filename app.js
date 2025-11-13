ece4890a03b1ed76ed88f7c51f9477a039278be8
asadbek <asadbekkhudayberdiyev07@gmail.com>
2025-11-14 00:41:00 +0900

    middleware

--- app.js
+++ app.js
@@ -7,12 +7,38 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json()); // JSON parsing middleware`
+app.use((req, res, next) => {
+	console.log('Middleware worked!');
+	next();
+}); // Middleware ishga tushuriladi USE mthodi orqali
+
+function adminCheck(req, res, next) {
+	const { email } = req.body;
+	if (email !== 'admin@gmail.com') {
+		return res.status(401).json({ message: 'USer is not admin' });
+	}
+	req.isAdmin = true;
+	next();
+}
+
 app.get('/', (req, res) => {
 	res.end('Hello asadbook-dev!');
 });
 
 app.get('/user', (req, res) => {
 	res.end('Hello user!');
+});
+
+app.post('/post', (req, res) => {
+	console.log(req.body);
+	res.json(req.body);
+});
+
+app.post('/admin-login', adminCheck, (req, res) => {
+	if (req.isAdmin) {
+		return res.json({ body: req.body, message: 'Login successfully!' });
+	}
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
